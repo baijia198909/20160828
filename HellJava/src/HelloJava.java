95856568e8e8e941737c23bafbfc9c70a924ccee
baijia <lu563795939@163.com>
2018-09-14 23:49:33 +0800

    aa

---
+++ HellJava/src/HelloJava.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ * @author baijia
+ * @date 2018/03/14
+ */
+public class HelloJava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello,Java");
+	}
+
+}
