750860544251565ca821a0590300fc5268b7914e
baijia <lu563795939@163.com>
2018-10-04 20:21:50 +0800

    init

---
+++ finance/src/main/java/com/huawei/b00314808/finance/MyAccountApplication.java
@@ -0,0 +1,12 @@
+package com.huawei.b00314808.finance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyAccountApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyAccountApplication.class, args);
+	}
+}
